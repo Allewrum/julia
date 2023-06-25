fa36b1fb02e06a74243c339400a129b303ed4c37
Allewrum <121388965+Allewrum@users.noreply.github.com>
2023-06-25 16:10:34 +0300

    Update Prac_5.jl

--- Prac_5.jl
+++ Prac_5.jl
@@ -1,4 +1,4 @@
-#Practic5###############################################################################################
+#5 Практика
 # Пузырьковая сортировка O( n^2 )
 function bubble_sort!(array::AbstractVector{T})::AbstractVector{T} where T <: Number
     n = length(array)
@@ -31,7 +31,6 @@ bubble_sort(array::AbstractVector)::AbstractVector = bubble_sort!(copy(array))
 function insert_sort!(array::AbstractVector{T})::AbstractVector{T} where T <: Number
     n = 1
     # Инвариант: срез array[1:n] - отсортирован
-
     while n < length(array) 
         n += 1
         i = n
@@ -40,10 +39,8 @@ function insert_sort!(array::AbstractVector{T})::AbstractVector{T} where T <: Nu
             array[i], array[i-1] = array[i-1], array[i]
             i -= 1
         end
-
         # Утверждение: array[1] <= ... <= array[n]
     end
-
     return array
 end
 
@@ -63,7 +60,6 @@ function comb_sort!(array::AbstractVector{T}, factor::Real=1.2473309) where T <:
         end
         step = Int(floor(step/factor))
     end
-
     # Теперь массив почти упорядочен, осталось сделать всего несколько итераций внешнего цикла в bubble_sort!(array)
     bubble_sort!(array)
 end
@@ -76,10 +72,8 @@ comb_sort(array::AbstractVector, factor::Real=1.2473309)::AbstractVector = comb_
 # Сортировка Шелла O( n^2 )
 function shell_sort!(array::AbstractVector{T})::AbstractVector{T} where T <: Number
     n = length(array)
-
 	# Здесь последовательность шагов прореживания массива определяется генератором
     step_series = (n÷2^i for i in 1:Int(floor(log2(n)))) 
-
     for step in step_series
         for i in firstindex(array):step-1
             insert_sort!(@view array[i:step:end]) # - сортировка вставками выделенного (прореженного) подмассива
@@ -143,9 +137,7 @@ end
 
 merge_sort(array::AbstractVector)::AbstractVector = merge_sort!(copy(array))
 
-
-
-# Использую модуль Random для генерации случайного массива
+# Модуль Random используется для генерации случайного массива
 using Random
 
 A = randperm(100000)[1:100000]
@@ -158,20 +150,11 @@ A = randperm(100000)[1:100000]
 @showtime quick_sort(A)
 
 #= Топ сортировок 
-bubble_sort(A): 9.740503 seconds (21.70 k allocations: 1.927 MiB, 0.14% compilation time)
-insert_sort(A): 1.242766 seconds (7.11 k allocations: 1.119 MiB, 0.82% gc time, 1.25% compilation time)
-comb_sort(A): 0.023961 seconds (20.77 k allocations: 1.875 MiB, 68.58% compilation time)
-shell_sort(A): 0.108704 seconds (124.69 k allocations: 7.244 MiB, 43.83% compilation time)
-merge_sort(A): 0.262546 seconds (831.54 k allocations: 38.854 MiB, 97.07% compilation time)
-quick_sort(A): 0.083688 seconds (375.24 k allocations: 18.645 MiB, 14.96% gc time, 89.28% compilation time)
+bubble_sort(A): 9.751607 seconds (21.70 k allocations: 1.927 MiB, 0.14% compilation time)
+insert_sort(A): 1.252866 seconds (7.11 k allocations: 1.119 MiB, 0.82% gc time, 1.25% compilation time)
+comb_sort(A): 0.026981 seconds (20.77 k allocations: 1.875 MiB, 68.58% compilation time)
+shell_sort(A): 0.109306 seconds (124.69 k allocations: 7.244 MiB, 43.83% compilation time)
+merge_sort(A): 0.267841 seconds (831.54 k allocations: 38.854 MiB, 97.17% compilation time)
+quick_sort(A): 0.081678 seconds (375.24 k allocations: 18.645 MiB, 14.96% gc time, 89.38% compilation time)
 =#
 
-@showtime sort(A,alg=InsertionSort)
-@showtime sort(A,alg=QuickSort)
-@showtime sort(A,alg=MergeSort)
-
-#= Библиотечные сортировки
-sort(A, alg = InsertionSort): 1.206621 seconds (63.45 k allocations: 4.079 MiB, 2.13% compilation time)
-sort(A, alg = QuickSort): 0.017171 seconds (15.88 k allocations: 1.670 MiB, 73.98% compilation time)
-sort(A, alg = MergeSort): 0.051416 seconds (155.36 k allocations: 8.990 MiB, 89.43% compilation time)
-=#
