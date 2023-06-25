8f23569c40b8ab05e9786f0593539f3c37cd53f7
Allewrum <121388965+Allewrum@users.noreply.github.com>
2023-06-25 16:13:28 +0300

    Update Prac_6.jl

--- Prac_6.jl
+++ Prac_6.jl
@@ -9,15 +9,12 @@ Base. +(a::Vector2D{T},b::Vector2D{T}) where T = Vector2D{T}(Tuple(a) .+ Tuple(b
 Base. -(a::Vector2D{T}, b::Vector2D{T}) where T = Vector2D{T}(Tuple(a) .- Tuple(b))
 Base. *(α::T, a::Vector2D{T}) where T = Vector2D{T}(α.*Tuple(a))
 
-# norm(a) - длина вектора, эта функция опредедена в LinearAlgebra
 LinearAlgebra.norm(a::Vector2D) = norm(Tuple(a))
 
-# dot(a,b)=|a||b|cos(a,b) - скалярное произведение, эта функция определена в LinearAlgebra
 LinearAlgebra.dot(a::Vector2D{T}, b::Vector2D{T}) where T = dot(Tuple(a), Tuple(b))
 
 Base. cos(a::Vector2D{T}, b::Vector2D{T}) where T = dot(a,b)/norm(a)/norm(b)
 
-# xdot(a,b)=|a||b|sin(a,b) - косое произведение
 xdot(a::Vector2D{T}, b::Vector2D{T}) where T = a.x*b.y-a.y*b.x
 
 Base.sin(a::Vector2D{T}, b::Vector2D{T}) where T = xdot(a,b)/norm(a)/norm(b)
@@ -26,10 +23,9 @@ Base.sign(a::Vector2D{T}, b::Vector2D{T}) where T = sign(sin(a,b))
 
 Segment2D{T <: Real} = NamedTuple{(:A, :B), NTuple{2,Vector2D{T}}}
 
-#= Отрисовка =#
 stored_lims = [0,0,0,0]
 
-function lims!(x1,y1,x2,y2)
+function limss!(x1,y1,x2,y2)
 	stored_lims[1] = min(x1-1,stored_lims[1])
 	stored_lims[2] = min(y1-1,stored_lims[2])
 	stored_lims[3] = max(x2+1,stored_lims[3])
@@ -39,7 +35,7 @@ function lims!(x1,y1,x2,y2)
 	ylims!(stored_lims[2], stored_lims[4])
 end
 
-lims!(x,y) = lims!(x,y,x,y)
+limss!(x,y) = limss!(x,y,x,y)
 
 function draw(vertices::AbstractArray{Vector2D{T}}) where T
 	vertices = copy(vertices)
@@ -78,18 +74,20 @@ end
 
 # 2. Написать функцию, проверяющую, лежат ли две заданные точки по одну сторону от заданной прямой (прямая задается некоторым содержащимся в ней отрезком).
 function oneside(P::Vector2D{T}, Q::Vector2D{T}, s::Segment2D{T})::Bool where T
-	# l - направляющий вектор прямой
+	# l - направляющий вектор является прямым.
 	l = s.B - s.A
 
-	# Тогда, точки , лежат по одну сторону от прямой <=> когда угол между векторами имеют один и тот же знак (отложены в одну и ту же сторону от прямой)
+	# Тогда, точки , лежат по одну сторону от прямой <=> когда угол между векторами имеют один и тот же знак.
 	return sin(l, P-s.A) * sin(l,Q-s.A) > 0
 end
 
 # 3. Написать функцию, проверяющую, лежат ли две заданные точки по одну сторону от заданной кривой (кривая задается уравнением вида F(x,y) = 0).
+
 oneside(F::Function, P::Vector2D, Q::Vector2D)::Bool =
 	( F(P...) * F(Q...) > 0 )
 
 # 4. Написать функцию, возвращающую точку пересечения (если она существует) двух заданных отрезков.
+
 isinner(P::Vector2D, s::Segment2D)::Bool =
 	(s.A.x <= P.x <= s.B.x || s.A.x >= P.x >= s.B.x) &&
 	(s.A.y <= P.y <= s.B.y || s.A.y >= P.y >= s.B.y)
@@ -103,7 +101,7 @@ function intersection(s1::Segment2D{T},s2::Segment2D{T})::Union{Vector2D{T},Noth
 
 	x,y = A\b
 
-	# Замечание: Если матрица A - вырожденная, то произойдет ошибка времени выполнения
+	#Если матрица A - вырожденная, то произойдет ошибка
 	if isinner((;x, y), s1)==false || isinner((;x, y), s2)==false
 		return nothing
 	end
@@ -113,13 +111,8 @@ end
 
 println("Пересечение: ",intersection( (A=(x=-1.0,y=-1.0),B=(x=1.0,y=2.0)) , (A=(x=1.0,y=-1.0),B=(x=-1.0,y=3.0)) ))
 
-"""Затем происходит вычисление алгебраической суммы углов между направлениями из заданной точки на каждые две соседние вершины многоугольника. 
-Для этого используется цикл for с итерацией от первого до последнего индекса массива polygon. 
-В каждой итерации вычисляется угол между векторами от текущей вершины polygon[i] до заданной точки point и от текущей вершины до следующей вершины polygon[i % lastindex(polygon) + 1]. 
-Вычисленные углы суммируются с переменной sum."""
-
 # 5. Написать функцию, проверяющую лежит ли заданная точка внутри заданного многоугольника.
-function isinside(point::Vector2D{T},polygon::AbstractArray{Vector2D{T}})::Bool where T
+function inside(point::Vector2D{T},polygon::AbstractArray{Vector2D{T}})::Bool where T
 	@assert length(polygon) > 2
 
 	sum = zero(Float64)
@@ -133,70 +126,6 @@ function isinside(point::Vector2D{T},polygon::AbstractArray{Vector2D{T}})::Bool 
 	return abs(sum) > π
 end
 
-println("Внутри: ",isinside( (x=0,y=0),[(x=0,y=1),(x=1,y=-1),(x=-1,y=-1)] ))
-println("Внутри: ",isinside( (x=5,y=0),[(x=0,y=1),(x=1,y=-1),(x=-1,y=-1)] ))
+println("Внутри: ",inside( (x=0,y=0),[(x=0,y=1),(x=1,y=-1),(x=-1,y=-1)] ))
+println("Внутри: ",inside( (x=5,y=0),[(x=0,y=1),(x=1,y=-1),(x=-1,y=-1)] ))
 
-
-
-draw([(x=-5,y=-5),
-(x=-5,y=5),
-(x=5,y=5),
-(x=5,y=-5)])
-
-draw((x=-1,y=0))
-
-draw((x=1,y=0))
-
-savefig("rect.png") 
-
-clear()
-
-draw( grekhom!( [ 
-	(x=0,y=0),
-	(x=5,y=1),
-	(x=4,y=3),
-	(x=1,y=9),
-	(x=0,y=5),
-	(x=1,y=4),
-	(x=-3,y=8),
-	(x=-5,y=2),
-	(x=-2,y=3),
-] ) )
-
-draw((x=0,y=0))
-draw((x=5,y=1))
-draw((x=4,y=3))
-draw((x=1,y=9))
-draw((x=0,y=5))
-draw((x=1,y=4))
-draw((x=-3,y=8))
-draw((x=-5,y=2))
-draw((x=-2,y=3))
-
-savefig("grekhom.png")
-
-clear()
-
-draw( jarvis!( [
-	(x=0,y=0),
-	(x=5,y=1),
-	(x=4,y=3),
-	(x=1,y=9),
-	(x=0,y=5),
-	(x=1,y=4),
-	(x=-3,y=8),
-	(x=-5,y=2),
-	(x=-2,y=3),
-] ) )
-
-draw((x=0,y=0))
-draw((x=5,y=1))
-draw((x=4,y=3))
-draw((x=1,y=9))
-draw((x=0,y=5))
-draw((x=1,y=4))
-draw((x=-3,y=8))
-draw((x=-5,y=2))
-draw((x=-2,y=3))
-
-savefig("jarvis.png")
